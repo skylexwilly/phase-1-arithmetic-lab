9d081874b37b2a01c1c240199d20c3ff2e55d966
skylexwilly <merngowilson2@gmail.com>
2024-07-11 15:51:32 +0300

    arithmeticLab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+
+let multiply=(num1*num2)
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+
+let max = Math.max(10, 15, 20, 5);
